e2d8fda4f402862c07c43db631a6d47d8b5fe5ba
mas6y6 <125225333+mas6y6@users.noreply.github.com>
2025-08-31 16:20:29 -0500

    h

---
+++ src/windows/editor.js
@@ -0,0 +1,62 @@
+const electron = require('electron');
+const windowmanager = require('../windowmanager');
+const rendering = require('../rendering');
+const path = require("node:path");
+const {ipcMain} = require("electron");
+
+async function openeditor() {
+    const isMac = process.platform === 'darwin';
+
+    window = new electron.BrowserWindow({
+        titleBarOverlay: isMac,
+        trafficLightPosition: isMac ? { x: 15, y: 13 } : undefined,
+        titleBarStyle: isMac ? 'hidden' : undefined,
+        autoHideMenuBar: true,
+
+        minWidth: 600,
+        minHeight: 600,
+        width: 600,
+        height: 600,
+        icon: rendering.getIcon(),
+        show: false,
+        frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: true,
+            preload: path.join(global.srcpath, "preloads", "login.js")
+        }
+    });
+
+    await windowmanager.windowmanagerins.register("login",window);
+
+    ipcMain.on('minimize-window', (event) => {
+        const win = electron.BrowserWindow.fromWebContents(event.sender);
+
+        if (win) win.minimize();
+    });
+
+    ipcMain.on('maximize-window', (event) => {
+        const win = electron.BrowserWindow.fromWebContents(event.sender);
+
+        if (win) {
+            if (win.isMaximized()) win.unmaximize();
+            else win.maximize();
+        }
+    });
+
+    ipcMain.on('close-window', (event) => {
+        const win = electron.BrowserWindow.fromWebContents(event.sender);
+        if (win) win.close();
+    });
+
+    await window.loadURL("moonlight://editor");
+
+    window.show();
+    window.setMenu(null);
+
+    window.webContents.openDevTools();
+}
+
+module.exports = {
+    openeditor,
+}
