22982a9774aa562227e8219332bed4db9113edd1
Batista <118333712+BatistaCakewalk@users.noreply.github.com>
2025-09-01 14:13:23 -0400

    I hate divs.

--- src/windows/projectselection.js
+++ src/windows/projectselection.js
@@ -13,7 +13,7 @@ async function openprojectselection() {
         titleBarStyle: isMac ? 'hidden' : undefined,
         autoHideMenuBar: true,
 
-        minWidth: 800,
+        minWidth: 1500,
         minHeight: 800,
         width: 1200,
         height: 800,
